79701b9f206d03c71b4f80373a71fbb98f9cc33e
BiigMoney <64580434+OKThomas1@users.noreply.github.com>
2022-10-26 03:13:15 -0700

    uwsgi

--- django_project/settings.py
+++ django_project/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ['django_secret']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False 
+DEBUG = True 
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '24.67.112.53']
 
